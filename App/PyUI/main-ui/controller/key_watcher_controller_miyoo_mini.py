74e6143f5f6c43647e1cd907352b7cfd28e638e2
Christopher Jacobs <chrisj951@gmail.com>
2025-10-05 10:55:12 -0500

    Improve battery life in miyoo mini flip
    PyUI cf8246933cdd7aac5dda3db466f1023ddb2e7570

--- App/PyUI/main-ui/controller/key_watcher_controller_miyoo_mini.py
+++ App/PyUI/main-ui/controller/key_watcher_controller_miyoo_mini.py
@@ -131,9 +131,17 @@ class KeyWatcherControllerMiyooMini(ControllerInterface):
 
 
     def get_input(self, timeoutInMilliseconds):
-        self.last_held_input = next(iter(self.held_controller_inputs), None)
-        return self.last_held_input
+        start_time = time.time()
+        timeout = timeoutInMilliseconds / 1000.0
 
+        self.last_held_input = next(iter(self.held_controller_inputs), None)
+
+        while self.last_held_input is None and (time.time() - start_time) < timeout:
+            time.sleep(0.032)  # 1/30 of a second delay
+            self.last_held_input = next(iter(self.held_controller_inputs), None)
+
+        return self.last_held_input
+    
     def clear_input_queue(self):
         pass
 
