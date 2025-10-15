a7fe72edc7fb337f84942818dcdcc8e20368fad9
cobaltgit <cobaltsecuremail312@proton.me>
2025-10-15 17:41:27 +0100

    add SSH+SFTP

---
+++ App/SSH/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DROPBEAR_KEY_DIR="/mnt/SDCARD/sprig/etc/ssh"
+
+cd $(dirname "$0")
+
+mkdir -p "$DROPBEAR_KEY_DIR"
+[ ! -f "$DROPBEAR_KEY_DIR/dropbear_rsa_host_key" ] && ./dropbearmulti dropbearkey -t rsa -f "$DROPBEAR_KEY_DIR/dropbear_rsa_host_key"
+[ ! -f "$DROPBEAR_KEY_DIR/dropbear_ecdsa_host_key" ] && ./dropbearmulti dropbearkey -t ecdsa -f "$DROPBEAR_KEY_DIR/dropbear_ecdsa_host_key"
+[ ! -f "$DROPBEAR_KEY_DIR/dropbear_ed25519_host_key" ] && ./dropbearmulti dropbearkey -t ed25519 -f "$DROPBEAR_KEY_DIR/dropbear_ed25519_host_key"
+
+./dropbearmulti dropbear \
+    -r "$DROPBEAR_KEY_DIR/dropbear_rsa_host_key" \
+    -r "$DROPBEAR_KEY_DIR/dropbear_ecdsa_host_key" \
+    -r "$DROPBEAR_KEY_DIR/dropbear_ed25519_host_key" \
+    -c "./ssh_wrapper"
--- App/adbd_wifi/launch.sh
+++ App/adbd_wifi/launch.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-mount -o bind /mnt/SDCARD/sprig/etc/profile /etc/profile
-
 cd $(dirname "$0")
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
 ./adbd &
