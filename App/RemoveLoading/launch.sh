9d677a3f76306c0c51d6cee4930b307a71656475
Ryan Sartor <ryanmsartor@gmail.com>
2025-10-19 23:16:40 -0500

    remove loading app from MinUI

---
+++ App/RemoveLoading/launch.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+DIR="$(dirname "$0")"
+cd "$DIR"
+
+{
+echo "attempting to remove loading junk"
+cp -r bin /tmp
+cp -r lib /tmp
+
+export PATH=/tmp/bin:$PATH
+export LD_LIBRARY_PATH=/tmp/lib:$LD_LIBRARY_PATH
+
+cd /tmp
+
+rm -rf customer squashfs-root customer.modified
+
+cp /dev/mtd6 customer
+
+unsquashfs customer
+if [ $? -ne 0 ]; then
+	echo "unsquash failed"
+	sync
+	exit 1
+fi
+
+sed -i '/^\/customer\/app\/sdldisplay/d' squashfs-root/main
+echo "patched main"
+
+mksquashfs squashfs-root customer.mod -comp xz -b 131072 -xattrs -all-root
+if [ $? -ne 0 ]; then
+	sync
+	exit 1
+fi
+
+dd if=customer.mod of=/dev/mtdblock6 bs=128K conv=fsync
+
+} &> ./log.txt
+
+mv "./config.json" "./config.disabled"
+reboot
