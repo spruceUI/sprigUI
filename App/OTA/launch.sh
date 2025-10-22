8cf368981e06f962aa6da74631f5ab920c5fce4e
Ryan Sartor <ryanmsartor@gmail.com>
2025-10-22 12:08:11 -0500

    OTA works now! further enhancements definitely possible, but it works.

--- App/OTA/launch.sh
+++ App/OTA/launch.sh
@@ -9,6 +9,10 @@ BRANCH=main
 # the size of the zipfile plus the size of the contents thereof.
 SPACE_REQUIRED=800
 
+# Will not copy certain files and folders into place if set to false.
+OVERWRITE_RA_CONFIGS=false
+OVERWRITE_PYTHON3_DIR=false
+OVERWRITE_THEMES_DIR=true
 
 ##### FUNCTION DEFINITIONS #####
 
@@ -39,7 +43,7 @@ is_branch_newer_than_device() {
 
     # get version file from target branch of sprigUI repo
     cd /tmp
-    if ! wget --tries=3 -O version https://raw.githubusercontent.com/spruceUI/sprigUI/$BRANCH/sprig/version ; then
+    if ! wget --tries=3 -O version "https://raw.githubusercontent.com/spruceUI/sprigUI/$BRANCH/sprig/version" ; then
         log_message "Unable to retrieve version file from $BRANCH branch of sprigUI repo. Aborting."
         return 1
     fi
@@ -49,6 +53,9 @@ is_branch_newer_than_device() {
     device_version="$(tr -d ' \n\r' < /mnt/SDCARD/sprig/version)"
 
     [ -z "$device_version" ] && device_version="0.0.0"  # allow OTA if version file missing
+
+    log_message "$BRANCH branch is on version $branch_version."
+    log_message "Current installation is on version $device_version."
 
     # split the versions into 3 numbers each for comparison
     A_1=$(echo "$branch_version" | cut -d. -f1)
@@ -64,30 +71,33 @@ is_branch_newer_than_device() {
         eval A=\$A_$i
         eval B=\$B_$i
         if [ "$A" -gt "$B" ]; then 
+            log_message "Branch version is newer. Proceeding with update."
             return 0
         elif [ "$A" -lt "$B" ]; then
+            log_message "Device is on newer version than $BRANCH branch. Aborting."
             return 1
         # else continue to next field in the version number
         fi
     done
+    log_message "Device is on same version as $BRANCH branch. No update needed. Aborting."
     return 1
 }
 
 download_target_branch() {
     cd /mnt/SDCARD
-    if wget --tries=3 -O $BRANCH.zip https://github.com/spruceUI/sprigUI/archive/refs/heads/$BRANCH.zip ; then
+    if wget --tries=3 -O "$BRANCH.zip" "https://github.com/spruceUI/sprigUI/archive/refs/heads/$BRANCH.zip" ; then
         log_message "Successfully downloaded $BRANCH branch zip file."
         return 0
     else
         log_message "Failed to download $BRANCH branch zip file. Aborting."
-        rm -f /mnt/SDCARD/$BRANCH.zip
-        rm -f
+        rm -f "/mnt/SDCARD/$BRANCH.zip"
+        rm -rf "/mnt/SDCARD/sprigUI-$BRANCH"
         return 1
     fi
 }
 
 extract_archive() {
-    if unzip -o /mnt/SDCARD/$BRANCH.zip -d /mnt/SDCARD ; then
+    if unzip -o "/mnt/SDCARD/$BRANCH.zip" -d /mnt/SDCARD ; then
         log_message "Archive extracted successfully."
         return 0
     else
@@ -97,13 +107,12 @@ extract_archive() {
 }
 
 preserve_user_emu_launch_settings() {
-
     log_message "Preserving user emu launch settings."
     for configjson in /mnt/SDCARD/Emu/*/config.json ; do
 
         emu_dir="$(dirname "$configjson")"
         emu_name="$(basename "$emu_dir")"
-        new_json="$("/mnt/SDCARD/sprigUI-$BRANCH/Emu/$emu_name/config.json")"
+        new_json="/mnt/SDCARD/sprigUI-$BRANCH/Emu/$emu_name/config.json"
 
         [ -f "$new_json" ] || continue    # Skip if new config doesn’t exist
 
@@ -120,6 +129,48 @@ preserve_user_emu_launch_settings() {
     done
 }
 
+prepare_new_version_for_transfer() {
+
+    log_message "Preparing new version files to replace existing installation"
+
+    new_dir="/mnt/SDCARD/sprigUI-$BRANCH"
+    new_ra_dir="$new_dir/RetroArch"
+    new_python3_dir="$new_dir/App/PyUI/python3.10"
+    new_themes_dir="$new_dir/Themes"
+
+    if [ "$OVERWRITE_RA_CONFIGS" = false ]; then
+        log_message "Will not overwrite RA configs."
+        rm -f "$new_ra_dir/retroarchV4.cfg"
+        rm -rf "$new_ra_dir/config"
+    fi
+
+    if [ "$OVERWRITE_PYTHON3_DIR" = false ]; then
+        log_message "Will not overwrite Python3.10 directory."
+        rm -rf "$new_python3_dir"
+    fi
+
+    if [ "$OVERWRITE_THEMES_DIR" = false ]; then
+        log_message "Will not overwrite Themes directory."
+        rm -rf "$new_themes_dir"
+    fi
+
+    rm -f "$new_dir/create_sprig_release.*" "$new_dir/main" "$new_dir/TODO.txt"
+}
+
+complete_installation() {
+
+    log_message "Killing main execution loop, powerbutton watchdog, and SSH."
+    killall -9 main powerbutton_watchdog.sh dropbearmulti # adbd    ### Keep adbd on for testing
+    umount /etc/profile >/dev/null 2>&1
+
+    log_message "Copying new sprigUI version over old version."
+    cp -rf /mnt/SDCARD/sprigUI-"$BRANCH"/* /mnt/SDCARD
+
+    log_message "Installation complete. Cleaning up."
+    rm -rf "/mnt/SDCARD/$BRANCH.zip" "/mnt/SDCARD/sprigUI-$BRANCH"
+
+    log_message "Update finished. Syncing and rebooting! happy gaming.........."
+}
 
 ##### MAIN EXECUTION #####
 
@@ -130,9 +181,12 @@ if does_device_have_sufficient_space && is_wifi_connected && is_branch_newer_tha
 
     log_message "All checks passed. Proceeding to download $BRANCH branch of sprigUI repo."
     
-    if download_target_branch; then
-        extract_archive
+    if download_target_branch && extract_archive; then
         preserve_user_emu_launch_settings
+        prepare_new_version_for_transfer
+        complete_installation
+        sync
+        reboot
     else
         exit 1
     fi
