cb6b4c32488209638079427fd3cd1be026e67db3
Christopher Jacobs <chrisj951@gmail.com>
2025-10-25 10:00:50 -0500

    Improve text handling on miyoo mini flip -- Add option to just display text
    -msgDisplay 'Test message' -msgDisplayTimeMs 2000
    
    5be9a23906dd0c8d3b92d3f387198fd566db8469

--- App/PyUI/launch.sh
+++ App/PyUI/launch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+../../miyoo285/app/freemma
+
 export LD_LIBRARY_PATH="/mnt/SDCARD/App/PyUI/libs/:/config/lib/:/customer/lib"
 export PYSDL2_DLL_PATH="/mnt/SDCARD/App/PyUI/libs"
 
@@ -9,4 +11,13 @@ export EGL_VIDEODRIVER=mmiyoo
 export SDL_MMIYOO_DOUBLE_BUFFER=1
 
 rm /mnt/SDCARD/App/PyUI/run.txt
-/mnt/SDCARD/App/PyUI/python3.10/bin/MainUI /mnt/SDCARD/App/PyUI/main-ui/mainui.py -device SPRIG_MIYOO_MINI_FLIP -logDir "/mnt/SDCARD/App/PyUI/logs" -pyUiConfig "/mnt/SDCARD/App/PyUI/py-ui-config.json" -cfwConfig "/mnt/SDCARD/Saves/sprig/sprig-config.json" >> /mnt/SDCARD/App/PyUI/run.txt 2>&1
+
+/mnt/SDCARD/App/PyUI/python3.10/bin/MainUI \
+    /mnt/SDCARD/App/PyUI/main-ui/mainui.py \
+    -device SPRIG_MIYOO_MINI_FLIP \
+    -logDir "/mnt/SDCARD/App/PyUI/logs" \
+    -pyUiConfig "/mnt/SDCARD/App/PyUI/py-ui-config.json" \
+    -cfwConfig "/mnt/SDCARD/Saves/sprig/sprig-config.json" \
+    "$@" >> /mnt/SDCARD/App/PyUI/run.txt 2>&1
+
+../../miyoo285/app/freemma
--- App/PyUI/main-ui/devices/device.py
+++ App/PyUI/main-ui/devices/device.py
@@ -315,8 +315,16 @@ class Device:
         return Device._impl.double_init_sdl_display()
 
     @staticmethod
-    def shrink_text_if_needed(text):
-        return Device._impl.shrink_text_if_needed(text)
+    def get_text_width_measurement_multiplier():
+        return Device._impl.get_text_width_measurement_multiplier()
+    
+    @staticmethod
+    def max_texture_width():
+        return Device._impl.max_texture_width()
+
+    @staticmethod
+    def get_guaranteed_safe_max_text_char_count():
+        return Device._impl.get_guaranteed_safe_max_text_char_count()
 
     @staticmethod
     def get_system_config():
