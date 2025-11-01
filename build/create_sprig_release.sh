e74fc4448a8469b8005318838bb934d63674897d
xXJSONDeruloXx <danielhimebauch@gmail.com>
2025-11-01 00:33:28 -0400

    organize build scripts, add gitignore, justfile

--- build/create_sprig_release.sh
+++ build/create_sprig_release.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/.."
 
 ARCHIVE_NAME="sprig"
 VERSION_FILE="sprig/version"
+DESTINATION_DIR="dist"
 VERSION=
 
 if [ ! -f "$VERSION_FILE" ]; then
     echo "Error: could not find file $VERSION_FILE"
-    read
     exit 1
 fi
 
@@ -16,25 +16,24 @@ VERSION=$(< "$VERSION_FILE")
 
 if [ "$VERSION" == "" ]; then
     echo "Error: failed to retrieve version from $VERSION_FILE"
-    read
     exit 1
 fi
 
-OUTPUT_7Z="${ARCHIVE_NAME}V${VERSION}.7z"
+mkdir -p "$DESTINATION_DIR"
+
+OUTPUT_7Z="${DESTINATION_DIR}/${ARCHIVE_NAME}V${VERSION}.7z"
 
 if [ -f "$OUTPUT_7Z" ]; then
     echo "Removing already existing $OUTPUT_7Z"
     rm "$OUTPUT_7Z"
 fi
 
-7z a -t7z -mx=9 -xr!.git* -x!.gitignore -x!.gitattributes -x!"$(basename "$0")" -x!create_sprig_release.bat -x!main -x!TODO.txt "$OUTPUT_7Z" *
+7z a -t7z -mx=9 -xr!.git* -xr!build -x!.gitignore -x!.gitattributes -x!justfile -x!main -x!TODO.txt "$OUTPUT_7Z" *
 
 if [ $? -ne 0 ]; then
     echo "Error: failed to create 7z archive"
-    read
     exit 1
 fi
 
 echo "7z archive $OUTPUT_7Z created successfully"
-read
 exit 0
