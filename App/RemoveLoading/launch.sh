7804be8ebb40a03a1ea4e4984ba6577ec5763b6a
Ryan Sartor <ryanmsartor@gmail.com>
2025-11-15 00:19:15 -0600

    stuff sun is gonna yell at me for

--- App/RemoveLoading/launch.sh
+++ App/RemoveLoading/launch.sh
@@ -41,7 +41,7 @@ cd "$DIR"
 
 	dd if=customer.mod of=/dev/mtdblock6 bs=128K conv=fsync
 
-} &> ./log.txt
+} &> /mnt/SDCARD/Saves/spruce/RemoveLoading.log
 
 mv "$DIR/config.json" "$DIR/config.disabled"
 reboot
